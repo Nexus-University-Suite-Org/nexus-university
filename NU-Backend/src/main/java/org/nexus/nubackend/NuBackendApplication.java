57cc8d69cddddece95c065ef1801045ea2ae142a
Tagoole Mugwa David <tagooledavid4@gmail.com>
2026-08-24 13:42:14 +0300

    Initialise backend with dependencies

---
+++ NU-Backend/src/main/java/org/nexus/nubackend/NuBackendApplication.java
@@ -0,0 +1,13 @@
+package org.nexus.nubackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NuBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(NuBackendApplication.class, args);
+    }
+
+}
---
+++ NU-Backend/src/test/java/org/nexus/nubackend/NuBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nexus.nubackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NuBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
